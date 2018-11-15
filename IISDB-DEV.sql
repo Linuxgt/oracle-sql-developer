c6c2c6324feddb5e869f80a451bf6646512a64b8
liuteng <liuteng0524@126.com>
2018-11-15 17:45:55 +0800

    查看表注释

--- IISDB-DEV.sql
+++ IISDB-DEV.sql
@@ -1,1 +1,9 @@
-select * from user_tables;
+-- 查看当前用户下的所有表
+select table_name from user_tables;
+
+-- 查看所有表注释
+
+select TABLE_NAME ,
+COLUMN_NAME ,
+COMMENTS ,
+ORIGIN_CON_ID  from user_col_comments where table_name in (select table_name from user_tables);
