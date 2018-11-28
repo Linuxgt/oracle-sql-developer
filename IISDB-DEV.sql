6e7f697909d88329a6882a6d0ce4374ee541332c
liuteng <liuteng0524@126.com>
2018-11-28 17:40:19 +0800

    查看个人信息修改历史

--- IISDB-DEV.sql
+++ IISDB-DEV.sql
@@ -7,3 +7,8 @@ select TABLE_NAME ,
 COLUMN_NAME ,
 COMMENTS ,
 ORIGIN_CON_ID  from user_col_comments where table_name in (select table_name from user_tables);
+
+-- 查看个人信息修改历史
+SELECT
+    *
+FROM crtfct_info_history WHERE sr_no_id=32;
